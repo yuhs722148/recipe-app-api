760aae6e7cccdc578b9cb86db7cdf1a27085f10d
yuhs722148 <ckm6688@gmail.com>
2023-09-14 15:31:13 +0800

    Configured database on Dockerfile

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """Add x and y and return the result."""
+    return x + y
+
+def subtract(x, y):
+    """Substract x from y and return the result."""
+    return y - x
