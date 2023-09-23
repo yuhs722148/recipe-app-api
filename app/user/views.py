7dbcf7ca0168b35fe81848fe8c5fb2e455ca6721
yuhs722148 <ckm6688@gmail.com>
2023-09-23 21:48:11 +0800

    Implemented manage user API and the tests.

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 """
 Views for the user API.
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -21,3 +21,14 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer  # 覆寫預設的serializer_class成自己的
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     # 使用restdramework的畫面，而不是django預設的
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
