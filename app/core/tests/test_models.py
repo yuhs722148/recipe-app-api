a114915f7c3b0997bacc7b1c8ff20c4adb4873e2
yuhs722148 <ckm6688@gmail.com>
2023-10-04 13:47:21 +0800

    Modify recipe model for images.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,9 @@
 """
 Test for models.
 """
+from unittest.mock import patch
 from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -90,3 +92,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
